19f089d1a230f08e23c63ee09631ba22a4e2ba61
Carl-Hugo Marcotte <carlhugo@gmail.com>
2026-02-23 01:02:52 -0500

    Add integration test project

---
+++ src/CognitiveForge.McpCategoryRouting.IntegrationTests/Basic/ProgramTest.cs
@@ -0,0 +1,15 @@
+﻿extern alias Basic;
+
+using BasicProgram = Basic::Program;
+
+namespace CognitiveForge.McpCategoryRouting.IntegrationTests.Basic;
+
+public class ProgramTest
+{
+    [Fact]
+    public async Task Should_start()
+    {
+        await using var app = new McpApplicationFactory<BasicProgram>();
+        await using var mcpClient = await app.CreateMcpClient();
+    }
+}
